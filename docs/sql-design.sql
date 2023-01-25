307ac0ef485976de05f251bce1a458338d68ebaa
YoonHan0 <dbsgksdud12@naver.com>
2023-01-25 18:17:19 +0900

    board #SQL

--- docs/sql-design.sql
+++ docs/sql-design.sql
@@ -9,9 +9,7 @@ SELECT * FROM user;
 insert INTO user VALUES(null, '윤한영', 'dbsgksdud@test.com', password('1234'), 'male', now());
 
 -- UPDATE
-UPDATE user
-SET name = '둘리', gender = 'female'
-WHERE no = 1;
+UPDATE user SET name = '둘리', gender = 'female' WHERE no = 1;
 
 -- login
 SELECT * FROM user WHERE email = 'dbsgksdud@test.com' AND password = password('1234');
@@ -19,3 +17,12 @@ SELECT * FROM user WHERE email = 'dbsgksdud@test.com' AND password = password('1
 SELECT * FROM guestbook;
 DESC guestbook;
 insert INTO guestbook VALUES(null, '윤한영', '1234', '안녕하세요', now());
+
+SELECT *
+FROM board;
+DESC board;
+
+SELECT *
+FROM user;
+
+ISERT INTO board VALUES(null, '오늘점심뭐먹', '제발 되라 부탁할께', 0, now(), 0, 0, 0, 1);
