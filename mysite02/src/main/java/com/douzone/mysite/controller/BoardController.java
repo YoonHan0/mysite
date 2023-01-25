5c5095c80a40eddc02da4d1408f52259e694424b
YoonHan0 <dbsgksdud12@naver.com>
2023-01-25 15:59:46 +0900

    mysite #board init

--- mysite02/src/main/java/com/douzone/mysite/controller/BoardController.java
+++ mysite02/src/main/java/com/douzone/mysite/controller/BoardController.java
@@ -6,15 +6,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.douzone.mysite.web.mvc.board.BoardActionFactory;
+import com.douzone.web.mvc.Action;
+import com.douzone.web.mvc.ActionFactory;
 
 public class BoardController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String actionName = request.getParameter("a");
+		
+		ActionFactory af = new BoardActionFactory(); 
+		Action action = af.getAction(actionName);
+		action.execute(request, response);
 	}
-
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
--- mysite02/src/main/java/com/douzone/mysite/controller/GuestController.java
+++ mysite02/src/main/java/com/douzone/mysite/controller/GuestController.java
@@ -7,8 +7,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.douzone.mysite.web.mvc.guest.GuestActionFactory;
-import com.douzone.web2.mvc.Action;
-import com.douzone.web2.mvc.ActionFactory;
+import com.douzone.web.mvc.Action;
+import com.douzone.web.mvc.ActionFactory;
 
 public class GuestController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
