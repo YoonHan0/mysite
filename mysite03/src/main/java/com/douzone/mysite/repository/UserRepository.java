51f6265e6667dc11e65c9219842cf268c7c22d48
YoonHan0 <dbsgksdud12@naver.com>
2023-01-31 16:17:50 +0900

    exception #final

--- mysite03/src/main/java/com/douzone/mysite/repository/UserRepository.java
+++ mysite03/src/main/java/com/douzone/mysite/repository/UserRepository.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
 
 import org.springframework.stereotype.Repository;
 
+import com.douzone.mysite.exception.UserRepositoryException;
 import com.douzone.mysite.vo.UserVo;
 
 @Repository
@@ -119,7 +120,7 @@ public class UserRepository {
 				vo.setGender(rs.getString(4));
 			}
 		} catch (SQLException e) {
-			System.out.println("error:" + e);
+			throw new UserRepositoryException(e.toString());
 		} finally {
 			try {
 				if(rs != null) {
