01fc14ae7a04791654ae790902c57d7e5c94745f
YoonHan0 <dbsgksdud12@naver.com>
2023-02-01 13:53:21 +0900

    MyBatis User 적용 #1

--- mysite03/src/main/java/com/douzone/mysite/repository/UserRepository.java
+++ mysite03/src/main/java/com/douzone/mysite/repository/UserRepository.java
@@ -5,6 +5,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.sql.DataSource;
 
@@ -25,126 +27,27 @@ public class UserRepository {
 	private SqlSession sqlSession;
 	
 	public void insert(UserVo vo) {
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		
-		try {
-			conn = dataSource.getConnection();
-			
-			String sql = "insert into user values (null, ?, ?, password(?), ?, now())";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, vo.getName());
-			pstmt.setString(2, vo.getEmail());
-			pstmt.setString(3, vo.getPassword());
-			pstmt.setString(4, vo.getGender());
-			
-			pstmt.executeUpdate();
-			
-		} catch (SQLException e) {
-			System.out.println("Error:" + e);
-		} finally {
-			try {
-				if(pstmt != null) {
-					pstmt.close();
-				}
-				
-				if(conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}		
+		sqlSession.insert("user.insert", vo);
 	}
 
-	public UserVo findByEmailAndPassword(UserVo vo) {
-		return sqlSession.selectOne("user.findByEmailAndPassword", vo);	//namespace.id, vo type이랑 user.xml에서의 ParameterType이랑 같아야함
-	}	
+	public UserVo findByEmailAndPassword(String email, String password) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("e", email);
+		map.put("p", password);
+		
+		return sqlSession.selectOne("user.findByEmailAndPassword", map);
+	}
+//	public UserVo findByEmailAndPassword(UserVo vo) {
+//		return sqlSession.selectOne("user.findByEmailAndPassword", vo);	//namespace.id, vo type이랑 user.xml에서의 ParameterType이랑 같아야함
+//	}	
 
 	public UserVo findByNo(Long no) {
-		UserVo vo = null;
-		
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-				
-		try {
-			conn = dataSource.getConnection();
-			
-			String sql = "select no, name, email, gender from user where no=?";
-			pstmt = conn.prepareStatement(sql);
-			
-			pstmt.setLong(1, no);
-
-			rs = pstmt.executeQuery();
-			
-			if(rs.next()) {
-				vo = new UserVo();
-				
-				vo.setNo(rs.getLong(1));
-				vo.setName(rs.getString(2));
-				vo.setEmail(rs.getString(3));
-				vo.setGender(rs.getString(4));
-			}
-		} catch (SQLException e) {
-			throw new UserRepositoryException(e.toString());
-		} finally {
-			try {
-				if(rs != null) {
-					rs.close();
-				}
-				if(pstmt != null) {
-					pstmt.close();
-				}
-				if(conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		return vo;
+		return sqlSession.selectOne("user.findByNo", no);
 	}
 
 	public void update(UserVo vo) {
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		System.out.println("===============" + vo);
-		try {
-			conn = dataSource.getConnection();
-			
-			if("".equals(vo.getPassword())) {
-				String sql = "update user set name=?, gender=? where no=?";
-				pstmt = conn.prepareStatement(sql);
-				
-				pstmt.setString(1, vo.getName());
-				pstmt.setString(2, vo.getGender());
-				pstmt.setLong(3, vo.getNo());
-			} else {
-				String sql = "update user set name=?, gender=?, password=password(?) where no=?";
-				pstmt = conn.prepareStatement(sql);
-				
-				pstmt.setString(1, vo.getName());
-				pstmt.setString(2, vo.getGender());
-				pstmt.setString(3, vo.getPassword());
-				pstmt.setLong(4, vo.getNo());
-			}
-			
-			pstmt.executeUpdate();
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		} finally {
-			try {
-				if(pstmt != null) {
-					pstmt.close();
-				}
-				if(conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}		
+		sqlSession.update("user.update", vo);
 	}
 }
+
+
