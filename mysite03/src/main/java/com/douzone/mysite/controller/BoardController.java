bf5091a57bbdf912cfd6854fd7a6a52866711efc
YoonHan0 <dbsgksdud12@naver.com>
2023-02-02 14:56:18 +0900

    aop: measure execution time

--- mysite03/src/main/java/com/douzone/mysite/controller/BoardController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/BoardController.java
@@ -2,10 +2,13 @@ package com.douzone.mysite.controller;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.douzone.mysite.service.BoardService;
 
@@ -25,5 +28,30 @@ public class BoardController {
 		
 		return "board/list";
 	}
+	
+	
+	
+//	
+//	@RequestMapping("/add")
+//	public String add(GuestbookVo vo) {		// name, password, message
+//		System.out.println("1===============" + vo);
+//		guestbookService.addMessage(vo);
+//		
+//		System.out.println("2===============" + vo);
+//		return "redirect:/guestbook";	// add 했으니까 작업 끝 Redirect!
+//	}
+//	
+//	@RequestMapping(value = "/delete", method=RequestMethod.GET)
+//	public String delete(GuestbookVo vo, HttpSession session, Model model) {
+//		
+//		model.addAttribute("no", vo.getNo());
+//		return "/guestbook/delete";
+//	}
+//	
+//	@RequestMapping(value="/delete", method=RequestMethod.POST)
+//	public String delete(GuestbookVo vo) {	// no, password
+//		guestbookService.deleteMessage(vo);
+//		return "redirect:/guestbook";		// delete 끗
+//	}
 
 }
--- mysite03/src/main/java/com/douzone/mysite/repository/GuestbookRepository.java
+++ mysite03/src/main/java/com/douzone/mysite/repository/GuestbookRepository.java
@@ -1,12 +1,6 @@
 package com.douzone.mysite.repository;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
+
 import java.util.List;
 import java.util.Map;
 
